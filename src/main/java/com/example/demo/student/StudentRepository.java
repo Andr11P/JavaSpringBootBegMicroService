52efa6482049d925b6fc0386a09a43550ab2a820
Andr11P <128001519+Andr11P@users.noreply.github.com>
2024-01-29 17:34:53 -0700

    Update StudentRepository.java

--- src/main/java/com/example/demo/student/StudentRepository.java
+++ src/main/java/com/example/demo/student/StudentRepository.java
@@ -8,6 +8,7 @@ import java.util.Optional;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
+    // postgreSQL query to select student information
     @Query("SELECT s FROM Student s WHERE s.email = ?1")
     Optional<Student> findStudentByEmail(String email);
 }
